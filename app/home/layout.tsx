e37096e793f70a33eb07722b1f52a590ae5d73e7
Cameron <cameron.cw@outlook.com>
2024-05-23 20:35:01 +0100

    Sign In Now Works

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,5 +1,22 @@
-export default function HomeLayout() {
-    return (
-        
-    )
+import { getServerSession } from "next-auth";
+import { ReactNode } from "react";
+import { authOptions } from "../utils/auth";
+import { redirect } from "next/navigation";
+
+export default async function HomeLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return redirect("/login");
+  }
+
+  return (
+    <div>
+        {children}
+    </div>
+  )
 }
