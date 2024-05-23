f10f3250f53b8e4d4c0bb41dd9d0b938d160e73f
Cameron <cameron.cw@outlook.com>
2024-05-23 21:45:18 +0100

    Home Page Styling

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,11 @@
+import MovieVideo from "../components/MovieVideo";
 import NavBar from "../components/Navbar";
 
 export default function HomePage() {
-  return <NavBar />;
+  return (
+    <div className="p-5 lg:p-0">
+      <MovieVideo />
+      <h1 className="text-3xl font-bold">Recently Added</h1>
+    </div>
+  );
 }
